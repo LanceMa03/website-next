8fb5c3404a8715d2dd8e115b48bb27b2dba7e017
Lance1738 <lancema507@gmail.com>
2025-03-25 17:32:55 -0400

    changed about section info

--- components/About.tsx
+++ components/About.tsx
@@ -27,39 +27,42 @@ export default function About() {
         </h1>
 
         <div className="flex flex-col md:flex-row items-start gap-x-4">
-          <div className="basis-1/3 md:shrink-0 space-y-4 text-lg flex-1">
+
+          <div className={`basis-1/3 md:shrink-0 space-y-4 text-lg flex-1 ${sourceSans.className}`}>
             <div>
               <strong>이름:</strong> 랑스 마
             </div>
             <div>
-              <strong>학교:</strong> 퍼듀 대학교
+              <strong>최애 라면:</strong> 신라면 🔥
             </div>
             <div>
-              <strong>나이:</strong> 21 살
+              <strong>최애 카페 음료:</strong> 말차 라떼 🍵
             </div>
+            {/* <div>
+              <strong>한국어 공부 기간:</strong> 2.5년 📚
+            </div> */}
             <div>
-              <strong>고향:</strong> 샌프란시스코, 캘리포니아
+              <strong>성격:</strong> 조용하지만 따뜻한 사람 😊
             </div>
+            
+            
             <Image src="/images/pfp.jpg" width={500} height={500} alt="Profile" className="w-72 h-72 object-cover rounded-2xl mt-6" />
           </div>
 
           <div className={`basis-2/3 text-lg leading-relaxed flex-1 space-y-4 text-justify ${sourceSans.className} font-sans`}>
            
-              <p>I&apos;m a student at Purdue University majoring in Computer Science with a minor in Korean Studies. Growing up in San Francisco,
-                    I&apos;ve been fascinated with the growing tech industry in front of my eyes. This, combined with my passion for
-                    computers and video games, I knew that I wanted to pursue my degree in computer science. 
-                    I&apos;m interested in software development and exploring how machine learning and AI can shape the future and
-                    impact people&apos;s day to day lives. 
+              <p>I&apos;m a student at Purdue University majoring in Computer Science with a minor in Korean Studies. Growing up in the Bay Area,
+                    I&apos;ve been fascinated with the evolving tech industry in front of my eyes. This, combined with my interest in video games and problem-solving skills is what drew me to computer science. 
+                    I&apos;m interested in software development and exploring how machine learning and AI can shape the future and impact people&apos;s day to day lives. 
                 </p>
     
-                <p>Outside the classroom, I&apos;m apart of The Data Mine where I am working with Lockheed Martin to develop a Generative Adversarial Network that uses AI/ML to conduct automated cyber anonomaly detection. Additionally, I&apos;m apart of VIP on the Education Technology Development team where I am assisting with
-                    the development of an &quot;Intro to Physical Computing&quot; course.
+                <p>Outside the classroom, I&apos;m currently working with The Data Mine to develop a GAN-based anonomaly detection framework to detect anomalies. Additionally, I&apos;m apart of VIP on the Education Technology Development team where I am working on the development of an &quot;Intro to Physical Computing&quot; course focused on embedded systems and microcontrollers. 
                 </p>
     
                 <p>I will also be a Software Engineering Intern at Capital One this summer.</p>
 
-                <p>You can usually find me in my room coding away into the night while listening to my favorite
-                    playlists on Spotify. If you don&apos;t find me coding, I&apos;ll probably be out and about travelling to a new city, on the beach watching the sunset.
+                <p>You can usually find me in my room mindlessly scrolling into night while listening to my favorite
+                    playlists on Spotify. Otherwise, I&apos;ll probably be out and about travelling to a new city, going for a walk, or watching the sunset somewhere.
                     Besides my love for coding and coffee, I also enjoy playing the occasional match of League of Legends and binge watching the latest hit k-drama(s) on Netflix.
                 </p>
     
