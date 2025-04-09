595650020d1d2499e4cbbc9c35e2604ce908c224
Lance1738 <lancema507@gmail.com>
2025-04-09 15:31:21 -0400

    test gh actions

--- src/app/korean/page.tsx
+++ src/app/korean/page.tsx
@@ -8,6 +8,7 @@ const notoSansKR = Noto_Sans_KR({
     display: "swap",
 });
 
+
 export default function KoreanPage() {
     return (
         <div className="min-h-screen flex flex-col justify-between items-center py-13 px-6 w-full h-full {backgroundColor: rgb(25, 27, 28))}">
